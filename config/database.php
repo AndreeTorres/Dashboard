2974d7c8ba61c9bbc38a04a7a4422bee1f2a696f
diegohdez33 <00007222@uca.edu.sv>
2025-11-08 21:22:28 -0600

    Fix database config for render

--- config/database.php
+++ config/database.php
@@ -84,17 +84,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', parse_url(env('DATABASE_URL', ''), PHP_URL_HOST)),
+            'port' => env('DB_PORT', parse_url(env('DATABASE_URL', ''), PHP_URL_PORT) ?: '5432'),
+            'database' => env('DB_DATABASE', ltrim(parse_url(env('DATABASE_URL', ''), PHP_URL_PATH), '/')),
+            'username' => env('DB_USERNAME', parse_url(env('DATABASE_URL', ''), PHP_URL_USER)),
+            'password' => env('DB_PASSWORD', parse_url(env('DATABASE_URL', ''), PHP_URL_PASS)),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
